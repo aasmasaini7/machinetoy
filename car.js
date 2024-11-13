fdca758217e1fb40eca7bec8165ed1834e77ced7
aasmasaini7 <156478358+aasmasaini7@users.noreply.github.com>
2024-11-12 23:59:04 -0500

    Update car.js

--- car.js
+++ car.js
@@ -1,4 +1,4 @@
-// information of the
+// information of the toy
 class ToyCar {
     constructor(model, color, scale, material, price, weight, dimensions, manufacturer, ageRecommendation, features, stockQuantity, isLimitedEdition, releaseDate, batteryOperated, category) {
         this.model = model;
